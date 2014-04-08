6b5bd4e189527fb9754ed4b8de529d4b7aad34aa
antoinedewailly <antoine.dewailly78@gmail.com>
2014-04-08 11:44:10 +0200

    classe films fait !
    toto

--- Projet Cinema GitHub/cinema/films.cs
+++ Projet Cinema GitHub/cinema/films.cs
@@ -7,5 +7,33 @@ namespace cinema
 {
     class films
     {
+        private string affiche;
+        private string nom;
+
+        public films(string nom, string affiche)
+        {
+            this.nom = nom;
+            this.affiche = affiche;
+        }
+
+        public string getAffiche()
+        {
+            return this.affiche;
+        }
+
+        public string getNom()
+        {
+            return this.nom;
+        }
+
+        public void setAffiche(string affiche)
+        {
+            this.affiche = affiche;
+        }
+
+        public void setNom(string nom)
+        {
+            this.nom = nom;
+        }
     }
 }
